fa9a4a02d00bd4ee9d64658d38366610997af264
Firen9 <boehm.sascha@hotmail.de>
2016-08-10 15:01:45 +0200

    GUI aber verbindung geht noch nicht

--- Gui.py
+++ Gui.py
@@ -1,4 +1,5 @@
 import tkinter
+import VerbindungsAufbau
 
 
 class MyGui(tkinter.Frame):
@@ -8,7 +9,28 @@ class MyGui(tkinter.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        # Eingabefelder
         self.ipEntry = tkinter.Entry(self)
         self.portEntry = tkinter.Entry(self)
         self.ipEntry.pack()
         self.portEntry.pack()
+        self.ip = tkinter.StringVar()
+        self.ip.set("127.0.0.1")
+        self.ipEntry["textvariable"] = self.ip
+        self.port = tkinter.StringVar()
+        self.port.set("21")
+        self.portEntry["textvariable"] = self.port
+        # Buttons
+        self.end = tkinter.Button(self)
+        self.end["text"] = "Stop"
+        self.end["command"] = self.quit
+        self.end.pack(side="right")
+        self.verbinden = tkinter.Button()
+        self.verbinden["text"] = "Verbindungsaufbau"
+        self.verbinden["command"] = VerbindungsAufbau.VerbindungsAufbau.aufbau(self.ip.get(), 21, "Hey", 5)
+        self.verbinden.pack(side="left")
+
+
+root = tkinter.Tk()
+app = MyGui(root)
+app.mainloop()
