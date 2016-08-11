1a1f8717cedea34badf7f89f05767efa21ff650d
Firen9 <boehm.sascha@hotmail.de>
2016-08-11 12:54:12 +0200

    VerbindungsAufbau mit with

--- VerbindungsAufbau.py
+++ VerbindungsAufbau.py
@@ -4,12 +4,7 @@ import socket
 # Klasse zum Verbingsaufbau und die dazu benötigten methode
 class VerbindungsAufbau():
     def aufbau(ip, port, nachricht, anzahl):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, port))
-        try:
-            i = 0
-            while i <= anzahl:
-                s.send(nachricht.encode())
-                i += 1
-        finally:
-            s.close()
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+            s.connect((ip,port))
+            for i in range(anzahl):
+                s.send((nachricht+"\n").encode())
