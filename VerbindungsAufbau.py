1fc23c6e17f177ea8f1062b13948618673b57cff
Firen9 <boehm.sascha@hotmail.de>
2016-08-11 14:07:10 +0200

    Exception Händling mit Connection fehler

--- VerbindungsAufbau.py
+++ VerbindungsAufbau.py
@@ -4,7 +4,10 @@ import socket
 # Klasse zum Verbingsaufbau und die dazu benötigten methode
 class VerbindungsAufbau():
     def aufbau(ip, port, nachricht, anzahl):
+      try:
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
             s.connect((ip,port))
             for i in range(anzahl):
                 s.send((nachricht+"\n").encode())
+      except ConnectionRefusedError:
+          print("Leider konnte keine Verbindung hergsetellt werden")
