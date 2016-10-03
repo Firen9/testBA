e5cf555688b9aada98a3619271da3208e7ea57f1
Firen9 <boehm.sascha@hotmail.de>
2016-10-03 22:40:34 +0200

    RawSocket hinzugefügbt

--- TestRawSocket.py
+++ TestRawSocket.py
@@ -30,7 +30,7 @@ def checksum(msg):
 # create a raw socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-except socket.error, msg:
+except socket.error as msg:
     print
     'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
